06602a7fe405e84e46c3a5fbf119e20e2903efc4
arbel <arbel.landau@gmail.com>
2026-04-18 22:20:43 +0300

    Update Arbel.java code

--- Arbel.java
+++ Arbel.java
@@ -1,7 +1,7 @@
 public class Arbel {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Arbel..." + i);
+            System.out.println("Arbelita..." + i);
         }
     }
 }
